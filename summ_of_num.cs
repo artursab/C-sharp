d09277df9d3a6ffb86b769e001fd4f39810a1534
artursab <107247506+artursab@users.noreply.github.com>
2023-10-07 23:39:12 +0200

    Add files via upload

---
+++ summ_of_num.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+
+    static int Foo(int value)
+    {
+        int result = 0;
+
+        while (value > 0)
+        {
+            result += value % 10;
+            value /= 10;
+        }
+        return result;
+    }
+
+
+    static void Main(string[] args)
+    {
+        int myValue = 561;
+        int result = Foo(myValue);
+    }
+}
