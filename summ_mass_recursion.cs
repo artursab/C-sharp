7076af2d8f44dfd80682beff9acd7fcecd423223
artursab <107247506+artursab@users.noreply.github.com>
2023-10-07 23:11:45 +0200

    Add files via upload

---
+++ summ_mass_recursion.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+class Program
+{
+
+    static int Sum(int[] myArray, int i = 0)
+    {
+        if (i >= myArray.Length) 
+            return 0;
+
+        return myArray[i] + Sum(myArray, i + 1);
+    }
+
+
+    static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 3, 4, 5 };
+
+        int result = Sum(myArray);
+    }
+}
