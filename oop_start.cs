7faeeff6d6a4d0c1fa5cb687424b6d12848ab1f3
artursab <107247506+artursab@users.noreply.github.com>
2023-10-08 22:04:08 +0200

    Add files via upload

---
+++ oop_start.cs
@@ -0,0 +1,33 @@
+﻿
+//OOP start
+
+class Point
+{
+    public int x;
+    public int y;
+    public Color color;
+}
+
+enum Color
+{
+    Red,
+    Green,
+    Yellow,
+    Blue
+}
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        Point point = new Point();
+        point.x = 1;
+        point.y = 2;
+        point.color = Color.Red;
+        
+        Console.WriteLine($"X: {point.x} | Y: {point.y} | Color: {point.color}");
+
+    }
+}
