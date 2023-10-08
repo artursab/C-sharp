17dba12ed53f6472a43ad3018f960f97484ac999
artursab <107247506+artursab@users.noreply.github.com>
2023-10-08 22:51:45 +0200

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_student
+{
+    class Student
+    {
+
+            public Guid id;
+            public string firstname;
+            public string lastname;
+            public string middlename;
+            public int age;
+            public string group;
+
+
+        public void Print()
+        {
+            Console.WriteLine("Informastion about student");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Lastname: {lastname}");
+            Console.WriteLine($"Name: {firstname}");
+            Console.WriteLine($"Middlename: {middlename}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Group: {group}");
+        }
+
+        public string GetFullName()
+        {
+            return $"{firstname}{lastname}{middlename}";
+        }
+
+    }
+}
---
+++ oop_student_dop.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace OOP_student
+{
+
+
+
+
+
+    class Program
+    {
+
+        static Student GetStudent()
+        {
+            var student = new Student();
+
+            student.firstname = "Maxim";
+            student.middlename = "Vanya";
+            student.lastname = "Kirilov";
+            student.age = 18;
+            student.id = Guid.NewGuid();
+            student.group = "GV11";
+
+            return student;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            var student1 = GetStudent();
+
+            student1.Print();
+
+            string fullname = student1.GetFullName();
+        }
+
+        
+    }
+
+
+}
+
