eaf8eae52b178654191196b06a479d2902040b98
artursab <107247506+artursab@users.noreply.github.com>
2023-10-08 18:13:41 +0200

    Add files via upload

---
+++ Var&linq.cs
@@ -0,0 +1,24 @@
+﻿
+
+// var and linq
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        var result = from i in numbers where i > 5 select i;
+
+        foreach (var item in result)
+        {
+            Console.Write($"{item}\t");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Type data results {result.GetType()}");
+    }
+}
+
