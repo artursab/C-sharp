0c1f83795bbd30449c4cb9a7f625a11be354d1c9
artursab <107247506+artursab@users.noreply.github.com>
2023-10-09 15:15:07 +0200

    Add files via upload

---
+++ Gun_isReloaded.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Gun
+{
+
+    class Gun
+    {
+
+        public Gun(bool isLoaded)
+        {
+            _isLoaded = isLoaded;
+        }
+
+        private bool _isLoaded;
+
+        private void Reload()
+
+        {
+            Console.WriteLine("Reloading...");
+
+            _isLoaded = true;
+
+            Console.WriteLine("Reloaded!");
+        }
+
+        public void Shoot()
+        {
+            if (_isLoaded)
+            {
+                Console.WriteLine("Gun isn't reloaded!");
+                Reload();
+            }
+
+            Console.WriteLine("Shoot - Shoot\n");
+            _isLoaded= false;
+        }
+
+    }
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun(isLoaded: true);
+            gun.Shoot();
+        }
+    }
+
+
+}
