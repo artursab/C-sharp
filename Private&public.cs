3588ad8ca4c41e95a714d8f42be725ba4666a300
artursab <107247506+artursab@users.noreply.github.com>
2023-10-09 14:02:25 +0200

    Add files via upload

---
+++ Private&public.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Private{
+
+
+    class Point
+    {
+        public int x = 1;
+
+        private int y = 5;
+
+        private void PrintX()
+        {
+            Console.WriteLine($"X: {x}");
+        }
+
+        public void PrintY()
+        {
+            Console.WriteLine($"Y: {y}");
+        }
+
+        public void PrintPoint()
+        {
+            PrintX();
+            PrintY();
+        }
+
+
+        class Program {
+
+
+            static void Main(string[] args)
+            {
+                Point point = new Point();
+
+                point.PrintX();
+
+            }
+        }
+
+
+        
+    }
+
+
+}
+
