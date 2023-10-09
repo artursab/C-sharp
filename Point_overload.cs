2f05054338645d6a7e23d426b3e5ca79a2650cd4
artursab <107247506+artursab@users.noreply.github.com>
2023-10-09 15:56:18 +0200

    Add files via upload

---
+++ Point_overload.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+namespace Overload
+{
+
+    class Point
+    {
+
+        public Point()
+        {
+            _x = _y = 1;
+        }
+
+        public Point(int x, int y)
+        {
+            _x = x; 
+            _y = y;
+        }
+
+
+
+    private int _x;
+    private int _y;
+
+    public void Print()
+    {
+        Console.WriteLine($"X: {_x}\tY: {_y}");
+    }
+
+        static void Main(string[] args)
+        {
+            Point point = new Point(0, 0);
+            point.Print();
+        }
+    }
+
+
+}
